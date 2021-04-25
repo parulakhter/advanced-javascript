f9d25cc48022239c1911a1af1a998e0b2ea2d580
parul akhter <parulakhter2020@.com>
2021-04-25 08:25:54 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,33 @@
+const age = 4;
+
+if(age){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+const name = 'suma';
+
+if(name){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+// falsy:
+
+// if value is 0 it will say false.
+// if value is empty string '' it will say false.
+// if value is undefined it will say false.
+// if value is null it will say false.
+// if value is NaN it will say false.
+// if value is false it will say false.
+
+// Truthy:
+
+// '0', ' ', [], {}, 'false'
+
+
+
